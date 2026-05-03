fda66bab4b1da0434f9502770894bd9b32edfc15
Andera Singgih Pratama <anderasinggih1@gmail.com>
2026-05-04 03:33:20 +0700

    refactor: convert welcome page to livewire component and unify public layout

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('welcome');
+Route::get('/', \App\Livewire\PublicWelcome::class)->name('welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified', 'relationship'])
